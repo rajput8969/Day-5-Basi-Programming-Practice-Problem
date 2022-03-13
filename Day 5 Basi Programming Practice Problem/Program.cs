505fd99713917308be4a0786eabf50c853297071
ANMOL <anmol999rajput@gmail.com>
2022-03-13 15:44:46 +0530

    Add project files.

---
+++ Day 5 Basi Programming Practice Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_5_Basi_Programming_Practice_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 5");
+        }
+    }
+}
