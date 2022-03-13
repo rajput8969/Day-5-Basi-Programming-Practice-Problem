164866f85c1418a2c5f85577de491656d97bbfe8
ANMOL <anmol999rajput@gmail.com>
2022-03-13 17:09:05 +0530

    Check Leap Year

--- Day 5 Basi Programming Practice Problem/Program.cs
+++ Day 5 Basi Programming Practice Problem/Program.cs
@@ -4,9 +4,21 @@ namespace Day_5_Basi_Programming_Practice_Problem
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Day 5 ");
+            int year;
+            Console.Write("Input an year : ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 400) == 0)
+                Console.WriteLine("{0} is a leap year.\n", year);
+            else if ((year % 100) == 0)
+                Console.WriteLine("{0} is not a leap year.\n", year);
+            else if ((year % 4) == 0)
+                Console.WriteLine("{0} is a leap year.\n", year);
+            else
+                Console.WriteLine("{0} is not a leap year.\n", year);
         }
     }
+    
 }
