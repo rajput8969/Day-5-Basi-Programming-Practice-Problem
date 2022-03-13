b5a601303f6c198de3ed8564c4d0896968f6804c
ANMOL <anmol999rajput@gmail.com>
2022-03-13 16:21:22 +0530

    Compute Quotient and Remainder

--- Day 5 Basi Programming Practice Problem/Program.cs
+++ Day 5 Basi Programming Practice Problem/Program.cs
@@ -6,7 +6,19 @@ namespace Day_5_Basi_Programming_Practice_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 5 ");
+            int dividend , divisor ;
+            Console.WriteLine("Enter a Dividend of Number : ");
+            dividend = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Divisor of Number : ");
+            divisor = int.Parse(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
         }
     }
 }
