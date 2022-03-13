128c6f1b36d97ddc17dcab6edf5dc93d21506305
ANMOL <anmol999rajput@gmail.com>
2022-03-13 16:01:47 +0530

    Check Vowel or consonant

--- Day 5 Basi Programming Practice Problem/Program.cs
+++ Day 5 Basi Programming Practice Problem/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿    //Program to Check Whether an Alphabet is Vowel or Consonant//
+using System;
 
 namespace Day_5_Basi_Programming_Practice_Problem
 {
@@ -6,7 +7,51 @@ namespace Day_5_Basi_Programming_Practice_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 5 ");
+
+            char ch;
+            Console.WriteLine("Enter any alphabet: ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'A':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'E':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'I':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'O':
+                    Console.WriteLine("vowel");
+                    break;
+                case 'U':
+                    Console.WriteLine("vowel");
+                    break;
+                default:
+                    Console.WriteLine("consonant");
+                    break;
+            }
+
+            Console.ReadLine();
+
         }
+
     }
 }
