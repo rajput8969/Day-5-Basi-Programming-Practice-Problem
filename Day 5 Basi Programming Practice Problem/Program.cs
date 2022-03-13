0c248f15788b610348b0e10fb755385675dd4e23
ANMOL <anmol999rajput@gmail.com>
2022-03-13 16:33:58 +0530

    Harmonic Calculation

--- Day 5 Basi Programming Practice Problem/Program.cs
+++ Day 5 Basi Programming Practice Problem/Program.cs
@@ -4,9 +4,21 @@ namespace Day_5_Basi_Programming_Practice_Problem
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Day 5 ");
+            int i, n;
+            double s = 0.0;
+
+            Console.Write("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+          
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1} \n", n, s);
         }
+
     }
 }
