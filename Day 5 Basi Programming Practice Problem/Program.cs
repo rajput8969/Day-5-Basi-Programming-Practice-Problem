b4f3ddcb76c73fbfc0405cad1d1e0956aa78cd3c
ANMOL <anmol999rajput@gmail.com>
2022-03-13 15:52:16 +0530

    3  Number Comparison

--- Day 5 Basi Programming Practice Problem/Program.cs
+++ Day 5 Basi Programming Practice Problem/Program.cs
@@ -6,7 +6,40 @@ namespace Day_5_Basi_Programming_Practice_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 5 ");
+            int first, second, third;
+
+            Console.WriteLine("Enter the first number : ");
+            first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number : ");
+            second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number : ");
+            third = int.Parse(Console.ReadLine());
+
+            if (first > second)
+            {
+          
+                if (first > third)
+                {
+                    Console.WriteLine("largest number : " + first);
+                }
+                else
+                {
+                    Console.WriteLine("largest number : " + third);
+                }
+            }
+            else
+            {
+                if (second > third)
+                {
+                    Console.WriteLine("largest number : " + second);
+                }
+                else
+                {
+                    Console.WriteLine("largest number : " + third);
+                }
+            }
         }
     }
 }
