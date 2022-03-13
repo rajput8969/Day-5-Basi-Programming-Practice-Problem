d87067cec1db084f2d189bd23998c75873b45d6b
ANMOL <anmol999rajput@gmail.com>
2022-03-13 16:12:43 +0530

    Swap number

--- Day 5 Basi Programming Practice Problem/Program.cs
+++ Day 5 Basi Programming Practice Problem/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿        //Program to Check Whether a Number is Even or Odd//
+using System;
 
 namespace Day_5_Basi_Programming_Practice_Problem
 {
@@ -6,7 +7,20 @@ namespace Day_5_Basi_Programming_Practice_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 5 ");
+            int number1, number2, temp;
+            Console.Write("\nInput the First Number : ");
+            number1 = int.Parse(Console.ReadLine());
+            Console.Write("\nInput the Second Number : ");
+            number2 = int.Parse(Console.ReadLine());
+           
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+            
+
+            Console.Write("\nFirst Number : " + number1);
+            Console.Write("\nSecond Number : " + number2);
+            Console.Read();
         }
     }
 }
