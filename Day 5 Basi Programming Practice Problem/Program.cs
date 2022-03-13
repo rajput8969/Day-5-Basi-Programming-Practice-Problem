9fbf6c1fdd3b40bd023ccca009349ed3c1cb5e48
ANMOL <anmol999rajput@gmail.com>
2022-03-13 16:27:57 +0530

    Factor

--- Day 5 Basi Programming Practice Problem/Program.cs
+++ Day 5 Basi Programming Practice Problem/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Print the prime factors of number N//
+using System;
 
 namespace Day_5_Basi_Programming_Practice_Problem
 {
@@ -6,7 +7,16 @@ namespace Day_5_Basi_Programming_Practice_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Day 5 ");
+            int a, b;
+            Console.WriteLine("Please enter your integer: ");
+            a = int.Parse(Console.ReadLine());
+            for (b = 1; b <= a; b++)
+            {
+                if (a % b == 0)
+                {
+                    Console.WriteLine(b + " is a factor of " + a);
+                }
+            }
         }
     }
 }
